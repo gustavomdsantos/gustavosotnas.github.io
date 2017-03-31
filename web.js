9209d2825ca4fb09301e259ca48f25d25df633a0
Gustavo Moraes <gustavosotnas1@gmail.com>
2017-03-31 13:59:15 -0300

    Cria arquivo 'web.js' para rodar servidor Node.js com arquivos estáticos
    (requer instalação do pacote "node-static":
    `npm install node-static`)

---
+++ web.js
@@ -0,0 +1,15 @@
+var static = require('node-static');
+var file = new static.Server('./assets', {
+    cache: 0,
+    gzip: true,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    }
+});
+ 
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(process.env.PORT || 9000);
