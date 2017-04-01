1705e8b193e679a6a143e47b5913689da21d973f
Gustavo Moraes <gustavosotnas1@gmail.com>
2017-04-01 01:05:40 -0300

    Modifica forma de referenciar super/subclasses no JS (apenas)

--- assets/js/views/app.js
+++ assets/js/views/app.js
@@ -1,12 +1,12 @@
 (function() {
   var App,
-    __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty;
 
   define(["jquery", "underscore", "backbone"], function($, _, Backbone) {});
 
-  App = (function(_super) {
-    __extends(App, _super);
+  App = (function(superClass) {
+    extend(App, superClass);
 
     function App() {
       return App.__super__.constructor.apply(this, arguments);
